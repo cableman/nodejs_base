4022d87e993dc79b8bb40f933ad8d24d33ee78ed
Jesper Kristensen <cableman@linuxdev.dk>
2014-09-11 07:52:38 +0200

    Added server (express) plugin to the mix

--- app.js
+++ app.js
@@ -7,6 +7,4 @@ var configPath = path.join(__dirname, "config.js");
 var config = architect.loadConfig(configPath);
 architect.createApp(config, function (err, app) {
   if (err) throw err;
-  console.log(app);
-  console.log("app ready");
 });
--- config.js
+++ config.js
@@ -6,5 +6,9 @@ module.exports = [
   {
     "packagePath": "./plugins/logger",
     "filename": "debug.log"
+  },
+  {
+    "packagePath": "./plugins/server",
+    "port": "3000"
   }
 ]
